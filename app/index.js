8e5c99c2537ba78099f7553d8e111dab2e0e9d98
Shu <shuaib.a.sajid@accenture.com>
2021-02-23 14:39:48 -0600

    Battle page

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom' // React can be used on many platforms like mob
                                 // we use react-dom to render to the DOM/Browser
 import './index.css'
 import Popular from './components/Popular'
+import Battle from './components/Battle'
 
 // Component
 // State
@@ -13,7 +14,7 @@ class App extends React.Component{
     render(){
         return (
         <div className='container'>
-           <Popular />
+           <Battle />
         </div>
         )
     }
